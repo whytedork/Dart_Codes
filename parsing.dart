1bac7d77a7332351ba40ab7e36d038cab0cedbd8
Hemant Gangwar <34183672+whytedork@users.noreply.github.com>
2020-08-29 16:56:01 +0530

    raw_string,parsing

---
+++ parsing.dart
@@ -0,0 +1,12 @@
+//parsing
+void main() {
+  var x = '12';
+  var y = '1.3';
+  var z = 3;
+  var a = int.parse(x);
+  var b = double.parse(y);
+  var c = z.toString();
+  print(a);
+  print(b);
+  print(c);
+}
---
+++ raw_string.dart
@@ -0,0 +1,6 @@
+void main() {
+  String s1 = r"hello my name is \n gangwar";
+  String s2 = "hello my name is \n gangwar";
+  print(s1);
+  print(s2);
+}
